fdd9d5035842ebc60efe2bb18a5bcaee90530fe6
rut503 <rutvik503@gmail.com>
2020-12-23 16:04:47 -0600

    added responsiveness to the book flip effect for all screen sizes.

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,123 +1,336 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import aboutMeStyles from "./aboutMe.module.scss"
 
 const AboutMe = () => {
-
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 	const [firstTwoPages, setFirstTwoPages] = useState(true)
+	const [showPageNumber, setShowPageNumber] = useState({
+		from: 1,
+		to: 1
+	})
+	const [classnamesForPages, setClassnamesForPages] = useState({
+		page1: undefined,
+		page2: undefined,
+		page3: undefined,
+		page4: undefined
+	})
+
+	useEffect(() => {
+		if(windowWidth < 768 ){   // screen size less than medium
+			if(showPageNumber.to > showPageNumber.from){ // going forward
+				console.log("Going Forward")
+				if(showPageNumber.to === 1){ // going to page 1
+					console.log("Page 1")
+					setClassnamesForPages({
+						page1: undefined,
+						page2: undefined,
+						page3: undefined,
+						page4: undefined
+					})
+				}
+				if(showPageNumber.to === 2){ // going to page 2
+					console.log("Page 2")
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: undefined,
+						page3: undefined,
+						page4: undefined,
+					})
+				}
+				else if(showPageNumber.to === 3){ // going to page 3
+					console.log("Page 3")
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: aboutMeStyles.flipPage,
+						page3: undefined,
+						page4: undefined,
+					})
+				}
+				else{ // going to page 4
+					console.log("Page 4")
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: aboutMeStyles.flipPage,
+						page3: aboutMeStyles.flipPage,
+						page4: undefined,
+					})
+				}
+			}
+			else{                                        // going backward
+				console.log("Going Backward")
+				if(showPageNumber.to === 1){ // going to page 1
+					console.log("Page 1")
+					setClassnamesForPages({
+						page1: aboutMeStyles.unflipPage,
+						page2: aboutMeStyles.unflipPage,
+						page3: aboutMeStyles.unflipPage,
+						page4: undefined					})
+				}
+				else if(showPageNumber.to === 2){ // going to page 2
+					console.log("Page 2")
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: aboutMeStyles.unflipPage,
+						page3: aboutMeStyles.unflipPage,
+						page4: undefined
+					})
+				}
+				else if(showPageNumber.to === 3){ // going to page 3
+					console.log("Page 3")
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: aboutMeStyles.flipPage,
+						page3: aboutMeStyles.unflipPage,
+						page4: undefined
+					})
+				}
+				else{
+					setClassnamesForPages({
+						page1: aboutMeStyles.flipPage,
+						page2: aboutMeStyles.flipPage,
+						page3: aboutMeStyles.flipPage,
+						page4: undefined
+					})
+				}
+			}
+		}
+		else{ // screen size more than medium
+			if(firstTwoPages){
+				setClassnamesForPages({
+					page1: undefined,
+					page2: aboutMeStyles.showRightSidePage,
+					page3: aboutMeStyles.hideLeftSidePage,
+					page4: undefined
+				})
+			}
+			else{
+				setClassnamesForPages({
+					page1: undefined,
+					page2: aboutMeStyles.hideRightSidePage,
+					page3: aboutMeStyles.showLeftSidePage,
+					page4: undefined
+				})
+			}
+		}
+	}, [showPageNumber, windowWidth, firstTwoPages])
+
+	useEffect(() => {
+		const getWindowWidth = () => {
+			setWindowWidth(window.innerWidth)
+		}
+		window.addEventListener("resize", getWindowWidth)
+		return () => {
+			window.removeEventListener("resize", getWindowWidth)
+		}
+	}, [])
 
 	return(
 		<div id="AboutMe" className={aboutMeStyles.container}>
 
 			<h1 className={aboutMeStyles.heading}>About Me</h1>
+			<p>{windowWidth}</p>
 
 			<div className={aboutMeStyles.largeBook}>
-				<div className={aboutMeStyles.page1}>
-					<div className={aboutMeStyles.erikaDP}></div>
-					<h1>I’m Erika Zocher, M.S.</h1>
 
-					<h3>Education</h3>
+				<div className={`${aboutMeStyles.page1} 
+								 ${classnamesForPages.page1}`}>
+					<div className={aboutMeStyles.imageErika}></div>
+					<p className={aboutMeStyles.pageTitle}>I’m Erika Zocher, M.S.</p>
 
-					<h1>Image of NC</h1>
-					<h2>Northland College</h2>
-					<ul>
-						<li>Aug 20__ - May 20__</li>
-						<li>Degree: B.S.</li>
-						<li>Major: Environmental Geosciences – Geology Emphasis</li>
-						<li>Minor: Biology</li>
-					</ul>
+					<div className={aboutMeStyles.scrollableDiv}
+						style={{height: "62%"}}>
+						<p className={aboutMeStyles.pageSubTitle}
+							style={{marginBottom: 15}}>Education</p>
+						
+						<div className={aboutMeStyles.flexyDiv}>
+							<div className={aboutMeStyles.imageNIU}></div>
+							<ul className={aboutMeStyles.pageText}>
+								<p className={aboutMeStyles.pageSubTitle}>Northern Illinois University</p>
+								<li style={{marginTop: 5}}>Aug 20__ - May 2020</li>
+								<li>Degree: M.S.</li>
+								<li>Major: Geology & Environmental Geosciences</li>
+							</ul>
+						</div>
 
-					<h1>Image of NIU</h1>
-					<h2>Northern Illinois University</h2>
-					<ul>
-						<li>Aug 20__ - May 2020</li>
-						<li>Degree: M.S.</li>
-						<li>Major: Geology & Environmental Geosciences</li>
-					</ul>
+						<div className={aboutMeStyles.flexyDiv}>
+							<div className={aboutMeStyles.imageNC}></div>
+							<ul className={aboutMeStyles.pageText} >
+								<p className={aboutMeStyles.pageSubTitle}>Northland College</p>
+								<li style={{marginTop: 5}}>Aug 20__ - May 20__</li>
+								<li>Degree: B.S.</li>
+								<li>Major: Environmental Geosciences</li>
+								<li style={{paddingLeft: 50}}>– Geology Emphasis</li>
+								<li>Minor: Biology</li>
+							</ul>
+						</div>
+					</div>
 				</div>
 
 				<div className={`${aboutMeStyles.page2} 
-								 ${firstTwoPages ? aboutMeStyles.showPage2 : aboutMeStyles.hidePage2}`}>
-					<h3>Presentations & Publications</h3>
+								 ${classnamesForPages.page2}`}>
+					<p className={aboutMeStyles.pageTitle}>My Presentations & Publications</p>
 
-					<h4>Master’s Thesis, 2020</h4>
-					<h6>Qualitative Perspectives on the Strange Trails of Persistence in STEM</h6>
-					<h6>Northern Illinois University</h6>
+					<div className={aboutMeStyles.scrollableDiv}>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Master’s Thesis, 2020</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Qualitative Perspectives on the Strange Trails of Persistence in STEM</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Northern Illinois University</p>
+						<hr/>
 
-					<h4>Virtual Oral Presentation, 2020</h4>
-					<h6>Qualitative Perspectives on the Strange Trails of Persistence in STEM </h6>
-					<h6>Geological Society of America North Central Sectional Meeting, Duluth, MN</h6>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Virtual Oral Presentation, 2020</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Qualitative Perspectives on the Strange Trails of Persistence in STEM </p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Geological Society of America North Central Sectional Meeting, Duluth, MN</p>
+						<hr/>
 
-					<h4>Oral Presentation, 2019</h4>
-					<h6>Demystifying Theoretical Frameworks for a Study of Persistence in STEM</h6>
-					<h6>Geological Society of America Annual Meeting, Phoenix, AZ</h6>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Oral Presentation, 2019</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Demystifying Theoretical Frameworks for a Study of Persistence in STEM</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Geological Society of America Annual Meeting, Phoenix, AZ</p>
+						<hr/>
 
-					<h4>Poster, 2019</h4>
-					<h6>A Pilot Study to Assess Chemistry Education in the Geosciences</h6>
-					<h6>Earth Educators’ Rendezvous, Nashville, TN</h6>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Poster, 2019</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>A Pilot Study to Assess Chemistry Education in the Geosciences</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Earth Educators’ Rendezvous, Nashville, TN</p>
+						<hr/>
 
-					<h4>Poster, 2019</h4>
-					<h6>A Pilot Study to Assess Chemistry Education in the Geosciences</h6>
-					<h6>Chicago Symposium, Naperville, IL</h6>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Poster, 2019</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>A Pilot Study to Assess Chemistry Education in the Geosciences</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Chicago Symposium, Naperville, IL</p>
+						<hr/>
 
-					<h4>Poster, 2019</h4>
-					<h6>Metals in the Mud: Changes in Surface Sediments Contamination in Long Island Sound</h6>
-					<h6>Geological Society of America North Central Sectional Meeting, Lincoln, NE, 2014: Geological 
-						Society of America Abstracts with Programs. Vol. 46, No. 4, p.10</h6>
+						<p className={aboutMeStyles.pageSubTitleSmall}>Poster, 2019</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Metals in the Mud: Changes in Surface Sediments Contamination in Long Island Sound</p>
+						<hr/>
+						<p className={aboutMeStyles.pageTextSmall}>Geological Society of America North Central Sectional Meeting, Lincoln, NE, 2014: Geological 
+							Society of America Abstracts with Programs. Vol. 46, No. 4, p.10</p>
+						<hr/>
+					</div>
 				</div>
 
 				<div className={`${aboutMeStyles.page3} 
-								 ${firstTwoPages ? aboutMeStyles.hidePage3 : aboutMeStyles.showPage3}`}>
-					<h3>My Rocky Journey</h3>
+								 ${classnamesForPages.page3}`}>
+					<p className={aboutMeStyles.pageTitle}>My Rocky Journey</p>
 
-					<h6>
-						I understand firsthand that there is not always a linear 
-						path in STEM.
-
-						I graduated with a geology degree from a small liberal arts 
-						college, a first-generation and low income student. I had 
-						great interest for my field, but also deficits and barriers 
-						that made it difficult to persist. In spite of the 
-						obstacles, I succeeded in achieving my degree and 
-						immediately began graduate school at a prestigious 
-						university to pursue geochemistry. Ultimately it was clear 
-						that it was not a good fit, and I left the program without 
-						my degree. However, this unexpected turn charted me on a 
-						course for the better.
-
-						The break from academia afforded me an opportunity to teach 
-						science to the public in various roles outside of what I 
-						had originally planned: as a museum facilitator, a 
-						nonprofit worker, and a high school educator. I later 
-						re-entered graduate school, this time joining a geoscience 
-						education research lab. Here, I 
-					</h6>
+					<div className={aboutMeStyles.scrollableDiv}>
+						<p className={aboutMeStyles.pageText}>
+							I understand firsthand that there is not always a linear 
+							path in STEM.
+						</p>
+						<p className={aboutMeStyles.pageText}>
+							I graduated with a geology degree from a small liberal arts 
+							college, a first-generation and low income student. I had 
+							great interest for my field, but also deficits and barriers 
+							that made it difficult to persist. In spite of the 
+							obstacles, I succeeded in achieving my degree and 
+							immediately began graduate school at a prestigious 
+							university to pursue geochemistry. Ultimately it was clear 
+							that it was not a good fit, and I left the program without 
+							my degree. However, this unexpected turn charted me on a 
+							course for the better.
+						</p>
+						<p className={aboutMeStyles.pageText}>
+							The break from academia afforded me an opportunity to teach 
+							science to the public in various roles outside of what I 
+							had originally planned: as a museum facilitator, a 
+							nonprofit worker, and a high school educator. I later 
+							re-entered graduate school, this time joining a geoscience 
+							education research lab. Here, I 
+						</p>
+						<p className={aboutMeStyles.pageText}>
+							The break from academia afforded me an opportunity to teach 
+							science to the public in various roles outside of what I 
+							had originally planned: as a museum facilitator, a 
+							nonprofit worker, and a high school educator. I later 
+							re-entered graduate school, this time joining a geoscience 
+							education research lab. Here, I 
+						</p>
+						<p className={aboutMeStyles.pageText}>
+							The break from academia afforded me an opportunity to teach 
+							science to the public in various roles outside of what I 
+							had originally planned: as a museum facilitator, a 
+							nonprofit worker, and a high school educator. I later 
+							re-entered graduate school, this time joining a geoscience 
+							education research lab. Here, I 
+						</p>
+					</div>
 				</div>
 
-				<div className={aboutMeStyles.page4}>
-					<h3>My Goal</h3>
+				<div className={`${aboutMeStyles.page4} 
+								 ${classnamesForPages.page4}`}>
+					<p className={aboutMeStyles.pageTitle}>My Goal</p>
 
-					<h6>
-						My goal is to inspire others to see that even their 
-						steepest goals are attainable with the right supports. 
-						STEM students and workers, especially those from 
-						underrepresented backgrounds, are all too often met with 
-						discouragement and feel a lack of a sense of belonging in 
-						their communities. Our current and future generations of 
-						scientists deserve an educational infrastructure that is 
-						designed for collaboration and one that celebrates a 
-						diversity of people and ideas.
+					<div className={aboutMeStyles.scrollableDiv}>
+						<p className={aboutMeStyles.pageText}>
+							My goal is to inspire others to see that even their 
+							steepest goals are attainable with the right supports. 
+							STEM students and workers, especially those from 
+							underrepresented backgrounds, are all too often met with 
+							discouragement and feel a lack of a sense of belonging in 
+							their communities. Our current and future generations of 
+							scientists deserve an educational infrastructure that is 
+							designed for collaboration and one that celebrates a 
+							diversity of people and ideas.
+						</p>
+						<p className={aboutMeStyles.pageText}>
+							My expertise in mentoring and student support can be widely 
+							applied to meet your academic or professional needs.
+						</p>
 
-						My expertise in mentoring and student support can be widely 
-						applied to meet your academic or professional needs.
-					</h6>
-
-					<h6><span>Let me help you Summit STEM!</span></h6>
+						<p className={aboutMeStyles.pageText} style={{textAlign: "center"}}>
+							<span>Let me help you Summit STEM!</span>
+						</p>
+					</div>
 				</div>
 			</div>
-			<button onClick={() => setFirstTwoPages(true)}>Prev</button>
-			<button onClick={() => setFirstTwoPages(false)}>Next</button>
-			<p>{firstTwoPages ? "Next Page : 1-2" : "Next Page : 3-4"}</p>
+
+			{/* ============================================================ */}
+			<button 
+				onClick={() => setFirstTwoPages(true)}
+				disabled={firstTwoPages ? true : false}
+			>
+				Prev
+			</button>
+			<button 
+				onClick={() => setFirstTwoPages(false)}
+				disabled={firstTwoPages ? false : true}
+			>
+				Next
+			</button>
+			<p>{firstTwoPages ? "Page : 1-2" : "Page : 3-4"}</p>
+			
+			{/* ============================================================ */}
+			
+			<button 
+				// onClick={() => setShowPageNumber(Math.abs((showPageNumber-1) % 4))}
+				onClick={() => setShowPageNumber({
+					from: showPageNumber.to, 
+					to: showPageNumber.to-1
+				})}
+				disabled={showPageNumber.to === 1 ? true : false}
+			>
+				Prev-S
+			</button>
+			<button 
+				onClick={() => setShowPageNumber({
+					from: showPageNumber.to, 
+					to: showPageNumber.to+1
+				})}
+				disabled={showPageNumber.to === 4 ? true : false}
+			>
+				Next-S
+			</button>
+			<p>Pre Page : {showPageNumber.from}</p>
+			<p>Current Page : {showPageNumber.to}</p>
 		</div>
 	)
 }
