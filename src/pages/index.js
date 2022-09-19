b8c2ef7fbe30ef01799174195e41df17e3b57e46
Rutvik Patel <34176344+rut503@users.noreply.github.com>
2022-09-18 22:57:24 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,10 @@ const MainPage = ({ location }) => {
 	return (
 		<Layout>
 			<Helmet>
-				<meta name="ahrefs-site-verification" content="dbeb69b3fb4f7f612c937a4e977e075e8a920677858863eab111fc929b6bd314">
+				
 				{/* -> Common Meta Tags (these can be defined in gatsby-config and imported using graphQL) */}
 				<title>Summit STEM</title>
+				<meta name="ahrefs-site-verification" content="dbeb69b3fb4f7f612c937a4e977e075e8a920677858863eab111fc929b6bd314">		
 				<meta name="description" content="Education consulting for STEM Majors" />
 				<meta name="keywords" content="STEM, Education, Resume Help, Consulting, College Applications, Cover Letter Help, Career Planning, Mock Interviews" />
 
